f2d71b70752e66ca591ec624e0f10012ae563f0e
DEEPAK DHAKA <124486673+deepakdhaka-1@users.noreply.github.com>
2026-01-11 14:08:01 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:n8n-nodes-base/community',
+  ],
+  rules: {
+    'n8n-nodes-base/node-param-default-missing': 'warn',
+    'n8n-nodes-base/node-param-description-missing': 'warn',
+  },
+};
