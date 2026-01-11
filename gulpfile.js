42980fff25d465fccd3a032e63a2509fda0d46f0
DEEPAK DHAKA <124486673+deepakdhaka-1@users.noreply.github.com>
2026-01-11 15:02:07 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 const { src, dest } = require('gulp');
 
 function buildIcons() {
-  return src('nodes/**/*.{png,svg}')
-    .pipe(dest('dist/nodes'));
+	return src('nodes/**/*.{png,svg}')
+		.pipe(dest('dist/nodes'));
 }
 
-exports.build = buildIcons;
 exports['build:icons'] = buildIcons;
