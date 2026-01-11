44e4000c4307c896e43719dc30b5ae91885a61be
DEEPAK DHAKA <124486673+deepakdhaka-1@users.noreply.github.com>
2026-01-11 15:02:26 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'plugin:n8n-nodes-base/community',
-  ],
-  rules: {
-    'n8n-nodes-base/node-param-default-missing': 'warn',
-    'n8n-nodes-base/node-param-description-missing': 'warn',
-  },
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:n8n-nodes-base/recommended',
+	],
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		'n8n-nodes-base/node-param-default-missing': 'off',
+		'n8n-nodes-base/node-param-description-missing-final-period': 'off',
+	},
 };
